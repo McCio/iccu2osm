9ef33529317196e6dc1c403a641103e6a8d9c26d
Marco Ciotola <848222@stud.unive.it>
2024-11-12 18:46:11 +0100

    feat: contacts cleanup, more filtering, start adressess

--- coalesce.py
+++ coalesce.py
@@ -2,27 +2,35 @@
 import polars as pl
 import pandas as pd
 
+def strip_strings(df):
+  return df.with_columns(df.select(pl.col(pl.Utf8).str.strip_chars()))
+
 contatti = pl.from_pandas(pd.read_xml('data/contatti.xml')).drop('denominazione', 'codice-sbn')
+contatti = strip_strings(contatti)
 fondi_speciali = pl.from_pandas(pd.read_xml('data/fondi-speciali.xml')).drop('denominazione')
+fondi_speciali = strip_strings(fondi_speciali)
 patrimonio = pl.from_pandas(pd.read_xml('data/patrimonio.xml')).drop('denominazione')
+patrimonio = strip_strings(patrimonio)
 biblioteche = pl.read_json('data/biblioteche.json')
-# biblioteche = biblioteche.select(pl.col('biblioteche').list.eval(pl.struct.unnest)).unnest('biblioteche')
 biblioteche = biblioteche.drop('metadati').explode('biblioteche').unnest('biblioteche').unnest('codici-identificativi').unnest('denominazioni').rename({'isil': 'codice-isil', 'ufficiale': 'denominazione', 'precedenti': 'denominazioni-precedenti', 'alternative': 'denominazioni-alternative'}).drop('indirizzo')
+biblioteche = strip_strings(biblioteche)
 territorio = pl.read_csv('data/territorio.csv', separator=';').drop('denominazione', 'codice-sbn')
+territorio = strip_strings(territorio)
 tipologie = pl.read_csv('data/tipologie.csv', separator=';').drop('denominazione biblioteca').rename({'codice isil': 'codice-isil'})
+tipologie = strip_strings(tipologie)
 
 def show_df(df):
   print(df.columns)
-  print(df)
+  #print(df)
 
-show_df(contatti)
-show_df(fondi_speciali)
-show_df(patrimonio)
-show_df(biblioteche)
-show_df(territorio)
-show_df(tipologie)
+#show_df(contatti)
+#show_df(fondi_speciali)
+#show_df(patrimonio)
+#show_df(biblioteche)
+#show_df(territorio)
+#show_df(tipologie)
 
-print(pl.sql("select f.* from fondi_speciali f where trim(f.\"fondo-speciale\") != ''").collect())
+#print(pl.sql("select f.* from fondi_speciali f where trim(f.\"fondo-speciale\") != ''").collect())
 
 complete = biblioteche.join(contatti, on='codice-isil', how='left', validate='1:1', coalesce=True)
 complete = complete.join(fondi_speciali, on='codice-isil', how='left', validate='1:1', coalesce=True)
@@ -35,7 +43,111 @@ complete = complete.with_columns(
   pl.when(reserved_access == True).then(pl.lit('permit')).when(reserved_access == False).then(pl.lit('yes')).alias('access'),
   pl.when(wheelchair_access == True).then(pl.lit('yes')).when(wheelchair_access == False).then(pl.lit('no')).alias('wheelchair'),
 ).drop('accesso')
-show_df(complete)
+complete = complete.with_columns(pl.when(pl.col('stato-registrazione').eq_missing(None)).then(pl.lit('Biblioteca censita')).otherwise('stato-registrazione').alias('stato-registrazione'))
+complete = complete.filter(
+  pl.col('stato-registrazione').ne(pl.lit('Biblioteca non più esistente')) &
+  pl.col('stato-registrazione').ne(pl.lit('Biblioteca non censita')) &
+  pl.col('stato-registrazione').ne(pl.lit('Altri istituti collegati ad attività dell\'ICCU')) &
+  ~pl.col('stato-registrazione').str.starts_with(pl.lit('Biblioteca confluita'))
+)
 
-print(pl.sql("select indirizzo, contatti, contatto, telefono, fax, email, url from complete").collect())
+#show_df(complete)
+
+
+############ START CONTATTI
+
+contatti = complete.explode('contatti').select('codice-isil', 'contatti').unnest('contatti')
+contatti.extend(complete.select('codice-isil', pl.lit('Telefono').alias('tipo'), pl.col('telefono').alias('valore'), pl.lit(None).alias('note')))
+contatti.extend(complete.select('codice-isil', pl.lit('E-mail').alias('tipo'), pl.col('email').alias('valore'), pl.lit(None).alias('note')))
+contatti.extend(complete.select('codice-isil', pl.lit('Fax').alias('tipo'), pl.col('fax').alias('valore'), pl.lit(None).alias('note')))
+contatti.extend(complete.select('codice-isil', pl.lit('Url').alias('tipo'), pl.col('url').alias('valore'), pl.lit(None).alias('note')))
+contatti.extend(complete.select('codice-isil', pl.lit(None).alias('tipo'), pl.col('contatto').alias('valore'), pl.lit(None).alias('note')))
+contatti = contatti.filter(pl.col('valore').ne_missing(None))
+contatti = contatti.with_columns(pl.col('valore').str.strip_chars(';:"/ '))
+contatti = strip_strings(contatti)
+contatti = contatti.with_columns(pl.col('valore').str.replace_all('^\\(?\\+\\s*3\\s*9[\\.:;\\s\\)]*', '+39 '))
+contatti = contatti.with_columns(pl.col('valore').str.replace_all('^\\+39\\s*$', ''))
+contatti = contatti.with_columns(pl.col('valore').str.replace_all('.*\\+54.*', ''))
+contatti = contatti.filter(pl.col('valore').ne(''))
+contatti = contatti.filter(pl.col('tipo').ne_missing('Telex') & pl.col('valore').str.contains('^\\d{1,6}($| ?[A-Z].+$)').not_())
+
+contatti = contatti.with_columns(pl.col('valore').str.replace_all('h+tt+ps?[;:]//+\\s*', 'https://'))
+contatti = contatti.with_columns(pl.when(pl.col('valore').str.to_lowercase().str.contains('^(http|www|[^/]+.(it|eu|com|org|net|site)(/|$))')).then(pl.lit('Url')).otherwise('tipo').alias('tipo'))
+contatti = contatti.with_columns(pl.col('valore').str.replace_all('https://', '', literal=True))
+contatti = contatti.with_columns(pl.when(pl.col('valore').str.contains('facebook', literal=True)).then(pl.lit('facebook'))
+  .when(pl.col('valore').str.contains('instagram', literal=True)).then(pl.lit('instagram'))
+  .when(pl.col('valore').str.contains('twitter', literal=True)).then(pl.lit('twitter'))
+  .otherwise('tipo').alias('tipo'))
+
+contatti = contatti.with_columns(pl
+  .when(pl.col('valore').str.starts_with('+39') & pl.col('tipo').ne_missing('Fax')).then(pl.lit('Telefono'))
+  .when(pl.col('valore').str.contains('^[03][\\d /-]{6,26}$') & pl.col('tipo').eq_missing(None)).then(pl.lit('Telefono'))
+  .otherwise('tipo').alias('tipo')
+)
+contatti = contatti.with_columns(pl.col('valore')
+  .str.replace_all('^\\+39\\s*\\+39\\s*', '+39 ')
+  .str.replace_all('^\\+37\\s*', '37')
+)
+contatti = contatti.with_columns(pl.when(pl.col('tipo').ne_missing('Telefono') & pl.col('tipo').ne_missing('Fax')).then('valore').otherwise(pl.col('valore').str.replace_all('[\\. \\(\\)]', '').str.replace('+39', '+39 ', literal=True)).alias('valore'))
+contatti = contatti.with_columns(pl.when((pl.col('tipo').ne_missing('Telefono') & pl.col('tipo').ne_missing('Fax')) | pl.col('valore').str.starts_with('+39')).then('valore').otherwise(pl.col('valore').str.replace('^', '+39 ')).alias('valore'))
+contatti = contatti.with_columns(
+  pl.when(pl.col('valore').str.contains('^\\+39 .+(int|dig)')).then(pl.col('valore').str.extract('(int(erno)?|digitare)(\\d+)', 0)).otherwise('note').alias('note'),
+  pl.when(pl.col('valore').str.contains('^\\+39 .+(int|dig)')).then(pl.col('valore').str.replace('(int(erno)?|digitare)(\\d+)$', '')).otherwise('valore').alias('valore'),
+)
+contatti = contatti.with_columns(
+  pl.when(pl.col('tipo').ne_missing('Telefono') & pl.col('tipo').ne_missing('Fax')).then('valore')
+  .otherwise(pl.col('valore').str.replace(' (\\d{1,6})(/|-)', ' ${1}'))
+  .alias('valore')
+)
+contatti = contatti.with_columns(
+  pl.when(pl.col('valore').str.contains('^\\+39 .+fax$')).then(pl.lit('Fax')).otherwise('tipo').alias('tipo'),
+  pl.when(pl.col('valore').str.contains('^\\+39 .+fax$')).then(pl.col('valore').str.replace('fax$', '')).otherwise('valore').alias('valore'),
+)
+contatti = contatti.with_columns(
+  pl.when(pl.col('note').str.contains('^[Ff]ax1?$')).then(None)
+  .otherwise('note').alias('note')
+)
+
+contatti = contatti.with_columns(pl.when(pl.col('valore').str.contains('@') & pl.col('tipo').ne_missing(pl.lit('PEC'))).then(pl.lit('E-mail')).otherwise('tipo').alias('tipo'))
+contatti = contatti.with_columns(pl.when(pl.col('valore').str.contains('@[a-z0-9\\.]*(pec|legalmail)[a-z0-9\\.]*\\.[a-z0-9\\.]+$')).then(pl.lit('PEC')).otherwise('tipo').alias('tipo'))
+
+contatti = contatti.with_columns(pl.when(pl.col('valore').str.starts_with('@') & pl.col('note').str.to_lowercase().str.contains('instagram', literal=True)).then(pl.lit('instagram')).otherwise('tipo').alias('tipo'))
+contatti = contatti.with_columns(pl.when(pl.col('valore').str.starts_with('@') & pl.col('note').str.to_lowercase().str.contains('twitter', literal=True)).then(pl.lit('twitter')).otherwise('tipo').alias('tipo'))
+contatti = contatti.with_columns(pl.when(pl.col('tipo').ne_missing('twitter')).then('valore').otherwise(pl.col('valore').str.replace('^(@|(https://)?(www\\.)?twitter\\.com/)', '').str.replace('(/|\\?).*$', '')).alias('valore'))
+contatti = contatti.filter((pl.col('tipo').eq_missing('instagram') & pl.col('valore').str.contains('/invites/contact', literal=True)).not_())
+contatti = contatti.with_columns(pl.when(pl.col('tipo').ne_missing('instagram')).then('valore').otherwise(pl.col('valore').str.replace('^(@|(https://)?(www\\.)?instagram\\.com/)', '').str.replace('(/|\\?).*$', '')).alias('valore'))
+contatti = contatti.with_columns(pl.when(pl.col('tipo').ne_missing('facebook')).then('valore').otherwise(pl.col('note').str.replace('^([Pp]agina\\s*|[Gg]ruppo\\s*|[Pp]rofilo\\s*)?[Ff]ace[Bb]+oo?k$', '').str.replace('(/|\\?).*$', '')).alias('note'))
+
+contatti = contatti.filter(~pl.col('valore').str.contains('^([\\d]{1,3}[\\./]){4}'))  # do not keep url if host is ip
+
+contatti = contatti.unique(subset=['codice-isil', 'tipo', 'valore']).sort('codice-isil', 'tipo', 'valore', nulls_last=True)
+contatti = contatti.with_columns(pl
+  .when(pl.col('tipo').eq_missing('Url')).then(pl.lit('contact_website'))
+  .when(pl.col('tipo').eq_missing('E-mail')).then(pl.lit('contact_email'))
+  .when(pl.col('tipo').eq_missing('PEC')).then(pl.lit('contact_pec'))
+  .when(pl.col('tipo').eq_missing('Fax')).then(pl.lit('contact_fax'))
+  .when(pl.col('tipo').eq_missing('Telefono')).then(pl.lit('contact_phone'))
+  .when(pl.col('tipo').eq_missing('facebook')).then(pl.lit('contact_facebook'))
+  .when(pl.col('tipo').eq_missing('instagram')).then(pl.lit('contact_instagram'))
+  .otherwise('tipo').alias('tipo')
+)
+contatti.write_excel('data/contatti.xlsx', 'contatti')
+
+contatti = contatti.filter(pl.col('tipo').ne_missing(None))
+contatti_grouped = contatti.group_by("codice-isil", "tipo").agg('valore', 'note').pivot(on='tipo', index='codice-isil',values=("valore", "note"))
+
+complete = complete.drop('contatti', 'contatto', 'url', 'fax', 'email', 'telefono')
+complete = complete.join(contatti_grouped, on='codice-isil', how='left', validate='1:1')
+
+###### FINE CONTATTI
+
+###### START ADDRESS
+# some "indirizzo" end with "Ed 2" or similar, meaning "building #2", that shouldn´t be extracted as housenumber
+complete = complete.with_columns(
+  pl.col('indirizzo').str.extract('\\s*,?\\s*(s\\.?n\\.?c?|\\d+[/\\a-hA-H-]*)\\s*$', 1).alias('address_housenumber'),
+  pl.col('indirizzo').str.replace('\\s*,?\\s*(s\\.?n\\.?c?|\\d+[/\\a-hA-H-]*)\\s*$', '').alias('address_street'),
+)
+
+
+
 complete.write_excel('data/complete.xlsx', 'biblioteche')
